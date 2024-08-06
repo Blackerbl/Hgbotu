bc63e0b6dfefbcaf332851799875f777b9ab3955
Blackerbl <162997554+Blackerbl@users.noreply.github.com>
2024-08-06 11:51:05 +0300

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
     intents: [
